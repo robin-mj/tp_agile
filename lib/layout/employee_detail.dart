99d12421f2be8fdbba82d681300ac9b3dc6a870c
Zhag <hugo.aub.etudes@gmail.com>
2023-03-17 13:23:34 +0100

    Affichage des données + début design

--- lib/layout/employee_detail.dart
+++ lib/layout/employee_detail.dart
@@ -1,11 +1,43 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:tp_agile/model/workers_model.dart';
 
 class EmployeeDetails extends StatelessWidget {
-  const EmployeeDetails({super.key});
+  final Worker worker;
+
+  const EmployeeDetails({Key? key, required this.worker}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      appBar: AppBar(
+        title: const Text('Modifier salarié'),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white, // fond blanc pour le padding arrondi
+                borderRadius: BorderRadius.circular(20.0), // bords arrondis
+              ),
+              child: Column(
+                children: [
+                  Text('${worker.firstname} ${worker.name}'),
+                  Text('Date d\'embauche : ${worker.startdate}'),
+                  Text('Salaire : ${worker.wages}€'),
+                  Text('Jours travaillés : ${worker.workingdays.join(', ')}'),
+                  Text('Équipe : ${worker.team}'),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
